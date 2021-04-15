5d267967d51a926a1996a2b241c518b33ae07f80
Izab3la <izabelamarek2004@gmail.com>
2021-04-15 20:26:59 +0200

    fix: update indeks.js

--- indeks.js
+++ indeks.js
@@ -1,28 +1,23 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+let slides = document.querySelectorAll('.slides')
+let currentSlide = 0;
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function showSlide(slideNumber) {
+    slides.forEach(slide => slide.style.display = 'none')
+    slides[slideNumber].style.display = 'inline'
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function prevSlide() {
+    if (--currentSlide < 0) currentSlide = slides.length - 1
+    showSlide(currentSlide)
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+function nextSlide() {
+    if (++currentSlide > slides.length - 1) currentSlide = 0
+    showSlide(currentSlide)
 }
+
+function init() {
+    showSlide(0)
+}
+
+init()
